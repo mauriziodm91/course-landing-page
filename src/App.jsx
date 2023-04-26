0f52f8ffa7bdbed5e1eb5dd0b192b4f3bc5b5fe6
Maurizio <mauriziodm91@gmail.com>
2023-04-25 23:16:28 -0300

    initial commmit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header/Header.component'
+import Topics from './components/Topics/Topics.component'
+import Info from './components/Info/Info.component'
+import Blog from './components/Blog/Blog.component'
+import Testimonials from './components/Testimonials/Testimonials.component'
+import Footer from './components/Footer/Footer.component'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Topics />
+      <Info />
+      <Blog />
+      <Testimonials />
+      <Footer />
+    </>
+  )
+}
+
+export default App
