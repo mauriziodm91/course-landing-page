c19a01346a1228a5316629b7af782bb3c7b6ad17
Maurizio <mauriziodm91@gmail.com>
2023-04-26 17:06:01 -0300

    created header component

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,20 @@
+// import Navbar from '../Navbar/Navbar.component' TODO
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='wrapper'>
+        <div className='cta'>
+          <p className='course-name'>Sound Design Masterclass</p>
+          <h1>Learn the Art of Sound Design</h1>
+          <a href='#' className='demo-btn'>
+            Demo Lesson
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
