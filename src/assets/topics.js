cffc0cf3b1d6dd58b351e804c825acb452dc62fc
Maurizio <mauriziodm91@gmail.com>
2023-04-27 15:28:37 -0300

    created topics component

---
+++ src/assets/topics.js
@@ -0,0 +1,33 @@
+import frequencies from './frequencies.png'
+import daw from './daw.png'
+import mixing from './mixing.png'
+import mastering from './mastering.png'
+import mixinconsole from './mixinconsole.png'
+import vocals from './vocals.png'
+
+export const topics = [
+  {
+    description: 'What are frequencies?',
+    image: frequencies,
+  },
+  {
+    description: 'Using DAW',
+    image: daw,
+  },
+  {
+    description: 'Vocals Processing',
+    image: vocals,
+  },
+  {
+    description: 'Mixing',
+    image: mixing,
+  },
+  {
+    description: 'Mixing Console',
+    image: mixinconsole,
+  },
+  {
+    description: 'Mastering',
+    image: mastering,
+  },
+]
---
+++ src/components/Topics/Topics.component.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { topics } from '../../assets/topics'
+import bgElement1 from '../../assets/bgElement1.svg'
+import './Topics.css'
+
+const Topics = () => {
+  const [currentImage, setCurrentImage] = useState(topics[0].image)
+
+  return (
+    <section id='topics' className='black'>
+      <div className='wrapper'>
+        <h2>What will you learn?</h2>
+        <div className='content-container'>
+          <ul>
+            {topics.map(({ description, image }, id) => (
+              <li key={id} onMouseEnter={() => setCurrentImage(image)}>
+                {description}
+              </li>
+            ))}
+          </ul>
+          <div className='topic-image'>
+            <img src={currentImage} />
+          </div>
+        </div>
+        <img src={bgElement1} className='bg-element-1' />
+      </div>
+    </section>
+  )
+}
+
+export default Topics
