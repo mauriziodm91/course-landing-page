a52df5277923b393bd96e00d594c841d81a734df
Maurizio <mauriziodm91@gmail.com>
2023-05-12 20:06:59 -0300

    created a mobile menu container inside Navbar component

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,26 +1,53 @@
+import MenuIcon from '../../assets/MenuIcon.svg'
+import Close from '../../assets/close.svg'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav>
-      <a href='#' className='logo'>
-        Sound<span className='red'>DZing</span>
-      </a>
-      <ul>
-        <li>
-          <a href='#'>Course Details</a>
-        </li>
-        <li>
-          <a href='#'>About</a>
-        </li>
-        <li>
-          <a href='#'>Blog</a>
-        </li>
-        <li>
-          <a href='#'>Testimonials</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <a href='#' className='logo'>
+          Sound<span className='red'>DZing</span>
+        </a>
+        <ul>
+          <li>
+            <a href='#topics'>Course Details</a>
+          </li>
+          <li>
+            <a href='#info'>About</a>
+          </li>
+          <li>
+            <a href='#blog'>Blog</a>
+          </li>
+          <li>
+            <a href='#testimonials'>Testimonials</a>
+          </li>
+        </ul>
+
+        <div className='menu-icon'>
+          <img src={MenuIcon} alt='' />
+        </div>
+      </nav>
+      <div className='mobile-menu-container'>
+        <div className='close-icon'>
+          <img src={Close} alt='' />
+        </div>
+        <ul className='menu-items'>
+          <li>
+            <a href='#topics'>Course Details</a>
+          </li>
+          <li>
+            <a href='#info'>About</a>
+          </li>
+          <li>
+            <a href='#blog'>Blog</a>
+          </li>
+          <li>
+            <a href='#testimonials'>Testimonials</a>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
